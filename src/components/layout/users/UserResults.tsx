fb8bd9c529b11852269a2ce0c17d5b3abf2e9991
Rob Merrill <robgmerrill@gmail.com>
2023-03-24 16:40:57 -0700

    bug fix

--- src/components/layout/users/UserResults.tsx
+++ src/components/layout/users/UserResults.tsx
@@ -33,7 +33,7 @@ function UserResults() {
   }
   return (
     <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-      {users && users.map(user => {
+      {Array.isArray(users) && users.map(user => {
         console.log("USER", user.login);
         return (
             <div>hi</div>
